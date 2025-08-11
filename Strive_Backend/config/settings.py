9592f595151524522dee22f66f61a75940e2afe5
Uzair <mukhan3223@gmail.com>
2025-08-11 11:53:49 +0500

    Modified comments

--- Strive_Backend/config/settings.py
+++ Strive_Backend/config/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
@@ -6,9 +5,6 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
