163dd275338c1df183b7bf42b20598cd10e02c26
Uzair <mukhan3223@gmail.com>
2025-08-12 12:35:30 +0500

    Adding custom user fields

--- Strive_Backend/users/admin.py
+++ Strive_Backend/users/admin.py
@@ -1,9 +1,25 @@
+# users/admin.py
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'role', 'is_active', 'is_staff')
-    search_fields = ('username', 'email', 'role')
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ("id", "phone", "first_name", "last_name", "role", "is_staff", "is_active")
+    search_fields = ("phone", "first_name", "last_name", "role")
+    ordering = ("id",)
+
+    fieldsets = (
+        (None, {"fields": ("phone", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("phone", "password1", "password2", "role", "first_name", "last_name", "is_staff", "is_active")
+        }),
+    )
