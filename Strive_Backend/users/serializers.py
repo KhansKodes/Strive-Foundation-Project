e1ce2f65d7f6e9cc52a7b86ccf041b466e00ca1b
Uzair <mukhan3223@gmail.com>
2025-08-12 12:40:17 +0500

    User/serializers file updated

--- Strive_Backend/users/serializers.py
+++ Strive_Backend/users/serializers.py
@@ -1,3 +1,4 @@
+# users/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
@@ -5,50 +6,61 @@ from django.contrib.auth.password_validation import validate_password
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-        validators=[validate_password],
-        style={'input_type': 'password'}
-    )
-    password2 = serializers.CharField(
-        write_only=True,
-        required=True,
-        style={'input_type': 'password'}
-    )
+    # We’ll accept multiple alias keys and normalize in validate()
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=False)  # alias for confirm
+    confirm_password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = User
-        fields = ('email', 'full_name', 'phone', 'password', 'password2', 'role')
-        extra_kwargs = {
-            'email': {'required': True},
-            'full_name': {'required': True},
-            'phone': {'required': True},
-            'role': {'required': True}
+        fields = (
+            "phone",
+            "first_name",
+            "last_name",
+            "role",
+            "password",
+            "password2",
+            "confirm_password",
+        )
+
+    def to_internal_value(self, data):
+        """
+        Map incoming spaced keys to model fields so your frontend can send:
+        {
+          "First name": "...",
+          "Last name": "...",
+          "conform password": "..."
         }
+        """
+        # clone dict so we don't mutate original
+        d = dict(data)
+
+        # normalize spaced keys
+        if "First name" in d:
+            d["first_name"] = d.pop("First name")
+        if "Last name" in d:
+            d["last_name"] = d.pop("Last name")
+        # handle misspelling/variant
+        if "conform password" in d and "confirm_password" not in d:
+            d["confirm_password"] = d.pop("conform password")
+
+        return super().to_internal_value(d)
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        pwd = attrs.get("password")
+        # allow either 'password2' or 'confirm_password'
+        pwd2 = attrs.pop("password2", None) or attrs.pop("confirm_password", None)
+        if pwd != pwd2:
+            raise serializers.ValidationError({"password": "Passwords must match."})
         return attrs
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop("password")
+        user = User.objects.create_user(password=password, **validated_data)
         return user
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'full_name', 'phone', 'role')
-        read_only_fields = ('email', 'role')
-
-
-class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(
-        required=True,
-        write_only=True,
-        style={'input_type': 'password'}
-    )
+        fields = ("id", "phone", "first_name", "last_name", "role")
