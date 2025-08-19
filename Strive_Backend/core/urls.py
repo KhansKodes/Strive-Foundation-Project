d2d657961c809ed99bdc2b0d0b268601e5629e5d
Uzair <mukhan3223@gmail.com>
2025-08-19 14:19:54 +0500

    Treatment cycle api added

--- Strive_Backend/core/urls.py
+++ Strive_Backend/core/urls.py
@@ -1,10 +1,18 @@
 from rest_framework.routers import DefaultRouter
-from .views import MediaItemViewSet, LegacyItemViewSet, ContactMessageViewSet, UrgentNeedViewSet
+from django.urls import path
+from .views import MediaItemViewSet, LegacyItemViewSet, ContactMessageViewSet, UrgentNeedViewSet, ImpactMetricViewSet, ImpactCardViewSet, ImpactOverviewView
 
 router = DefaultRouter()
 router.register(r'media', MediaItemViewSet, basename='media')
 router.register(r'legacy', LegacyItemViewSet, basename='legacy')
 router.register(r'contact', ContactMessageViewSet, basename='contact')
 router.register(r'urgent-need', UrgentNeedViewSet, basename='urgent-need')
+router.register(r'impact-metric', ImpactMetricViewSet, basename='impact-metric')
+router.register(r'impact-card', ImpactCardViewSet, basename='impact-card')
+
+urlpatterns = [
+    *router.urls,
+    path('impact-overview/', ImpactOverviewView.as_view(), name='impact-overview'),
+]
 
 urlpatterns = router.urls
