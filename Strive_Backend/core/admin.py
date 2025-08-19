881d42e5a2f7b5fc6e9354ec6bcd19d0f6d293be
Uzair <mukhan3223@gmail.com>
2025-08-19 14:21:13 +0500

    Sponsered money api added

--- Strive_Backend/core/admin.py
+++ Strive_Backend/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MediaItem, LegacyItem, ContactMessage, UrgentNeed
+from .models import MediaItem, LegacyItem, ContactMessage, UrgentNeed, ImpactMetric, ImpactCard
 
 admin.site.register(MediaItem)
 admin.site.register(LegacyItem)
@@ -12,3 +12,17 @@ class UrgentNeedAdmin(admin.ModelAdmin):
     list_filter = ("is_active",)
     search_fields = ("title", "description")
     ordering = ("priority", "-created_at")
+
+@admin.register(ImpactMetric)
+class ImpactMetricAdmin(admin.ModelAdmin):
+    list_display = ("key", "prefix", "value_number", "unit", "suffix", "label_line1", "label_line2", "display_order", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("key", "label_line1", "label_line2")
+    ordering = ("display_order", "id")
+
+@admin.register(ImpactCard)
+class ImpactCardAdmin(admin.ModelAdmin):
+    list_display = ("title", "emphasis_label", "emphasis_value", "display_order", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("title", "body", "emphasis_label", "emphasis_value")
+    ordering = ("display_order", "id")    
