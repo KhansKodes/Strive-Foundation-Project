1959ed8fce4bd3927cc09965502c41cc731f6d1f
Uzair <mukhan3223@gmail.com>
2025-08-20 11:17:57 +0500

    Get involved url  adeed

--- Strive_Backend/core/urls.py
+++ Strive_Backend/core/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from .views import MediaItemViewSet, LegacyItemViewSet, ContactMessageViewSet, UrgentNeedViewSet, ImpactStatsViewSet, ImpactTextBoxViewSet, ImpactStatsLatestView
+from .views import MediaItemViewSet, LegacyItemViewSet, ContactMessageViewSet, UrgentNeedViewSet, ImpactStatsViewSet, ImpactTextBoxViewSet, ImpactStatsLatestView, GetInvolvedViewSet
 
 router = DefaultRouter()
 router.register(r'media', MediaItemViewSet, basename='media')
@@ -9,6 +9,7 @@ router.register(r'contact', ContactMessageViewSet, basename='contact')
 router.register(r'urgent-need', UrgentNeedViewSet, basename='urgent-need')
 router.register(r'impact/stats', ImpactStatsViewSet, basename='impact-stats')
 router.register(r'impact/texts', ImpactTextBoxViewSet, basename='impact-texts')
+router.register(r'get-involved', GetInvolvedViewSet, basename='get-involved')
 
 impact_texts_list = ImpactTextBoxViewSet.as_view({'get': 'list'})
 
