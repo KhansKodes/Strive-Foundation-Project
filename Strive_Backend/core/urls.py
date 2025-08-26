517e131797d20ad41dfccb4719c54379c9ff15ee
Uzair <mukhan3223@gmail.com>
2025-08-26 10:25:21 +0500

    Strap line URL updated

--- Strive_Backend/core/urls.py
+++ Strive_Backend/core/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import (MediaItemViewSet, LegacyItemViewSet, ContactMessageViewSet, 
 UrgentNeedViewSet, ImpactStatsViewSet, ImpactTextBoxViewSet,
  ImpactStatsLatestView, GetInvolvedViewSet, IprcItemViewSet,
- EventViewSet, EventDetailBySlugView, EventDetailAdminViewSet, EventImageViewSet, DebugDataView)
+ EventViewSet, EventDetailBySlugView, EventDetailAdminViewSet,
+  EventImageViewSet, DebugDataView, StraplineViewSet, StraplineLatestView)
 
 router = DefaultRouter()
 router.register(r'media', MediaItemViewSet, basename='media')
@@ -17,6 +18,7 @@ router.register(r'legacy/iprc', IprcItemViewSet, basename='legacy-iprc')
 router.register(r'legacy/events', EventViewSet, basename='legacy-events')
 router.register(r'legacy/event-details', EventDetailAdminViewSet, basename='legacy-event-details')  # admin write
 router.register(r'legacy/event-images', EventImageViewSet, basename='legacy-event-images')          # admin write
+router.register(r'straplines', StraplineViewSet, basename='straplines')
 # Register general legacy route last
 router.register(r'legacy', LegacyItemViewSet, basename='legacy')
 
@@ -30,4 +32,5 @@ urlpatterns = [
     path('impact/texts-debug/', impact_texts_list),
     path('legacy/events/slug/<slug:slug>/detail/', EventDetailBySlugView.as_view(), name='legacy-event-detail-by-slug'),
     path('debug/data/', DebugDataView.as_view(), name='debug-data'),
+    path('straplines/latest/', StraplineLatestView.as_view(), name='strapline-latest'),
 ]
