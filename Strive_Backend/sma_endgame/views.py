a98898633dfaeb27cb2b25985b70f6136f067de5
Uzair <mukhan3223@gmail.com>
2025-08-28 10:48:28 +0500

    views  updated for Sma endgame

--- Strive_Backend/sma_endgame/views.py
+++ Strive_Backend/sma_endgame/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import EndgamePage
+from .serializers import EndgamePageSerializer
 
-# Create your views here.
+class EndgamePublicView(APIView):
+    authentication_classes = []  # no auth
+    permission_classes = []      # public
+
+    def get(self, request):
+        page = (EndgamePage.objects
+                .filter(is_active=True)
+                .order_by("-updated_at")
+                .first())
+
+        if not page:
+            return Response({"detail": "No Endgame content found."}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response(EndgamePageSerializer(page).data)
