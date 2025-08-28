b498a41f3ccb20102d41e658cfd447f606c41227
Uzair <mukhan3223@gmail.com>
2025-08-28 10:46:25 +0500

    Admin panel updated for Sma endgame

--- Strive_Backend/sma_endgame/admin.py
+++ Strive_Backend/sma_endgame/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import EndgamePage, Objective
 
-# Register your models here.
+class ObjectiveInline(admin.TabularInline):
+    model = Objective
+    extra = 1
+    fields = ("order", "title", "description")
+    ordering = ("order",)
+
+@admin.register(EndgamePage)
+class EndgamePageAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_active", "plan_url", "updated_at")
+    list_filter = ("is_active",)
+    search_fields = ("title", "subtitle", "plan_heading", "plan_subheading")
+    inlines = [ObjectiveInline]
+
+    # Optional: limit to one active record (you can delete this if you prefer multiple)
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if obj.is_active:
+            EndgamePage.objects.exclude(pk=obj.pk).update(is_active=False)
