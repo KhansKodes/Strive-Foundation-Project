3da9c73826f3a8024942bd61231336d9bd4d4984
Uzair <mukhan3223@gmail.com>
2025-08-29 15:35:45 +0500

    views added

--- Strive_Backend/partners/views.py
+++ Strive_Backend/partners/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import PartnerSection
+from .serializers import PartnerSectionSerializer
 
-# Create your views here.
+class PartnerPublicView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        section = (PartnerSection.objects
+                   .filter(is_active=True)
+                   .order_by("-updated_at")
+                   .first())
+        if not section:
+            return Response({"detail": "No partner section found."}, status=status.HTTP_404_NOT_FOUND)
+
+        ser = PartnerSectionSerializer(section, context={"request": request})
+        return Response(ser.data)
