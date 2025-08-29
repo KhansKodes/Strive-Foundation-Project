88efa61f7c481d0b4867ee128577c4b6c8ec5db9
Uzair <mukhan3223@gmail.com>
2025-08-29 15:34:25 +0500

    partners admin

--- Strive_Backend/partners/admin.py
+++ Strive_Backend/partners/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import PartnerSection, PartnerLogo
 
-# Register your models here.
+class PartnerLogoInline(admin.TabularInline):
+    model = PartnerLogo
+    extra = 1
+    fields = ("order", "image", "alt_text", "link_url", "is_active")
+    ordering = ("order",)
+
+@admin.register(PartnerSection)
+class PartnerSectionAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_active", "updated_at")
+    list_filter = ("is_active",)
+    search_fields = ("title",)
+    inlines = [PartnerLogoInline]
+
+    # Optional: only one active section at a time
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if obj.is_active:
+            PartnerSection.objects.exclude(pk=obj.pk).update(is_active=False)
