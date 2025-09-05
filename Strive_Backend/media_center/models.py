61d449e814d075031354b809c5f833ce0eeba5bf
Uzair <mukhan3223@gmail.com>
2025-09-05 10:37:59 +0500

    Media url updated

--- Strive_Backend/media_center/models.py
+++ Strive_Backend/media_center/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
-
+from django.core.exceptions import ValidationError
 
 class MediaPost(models.Model):
     class PostType(models.TextChoices):
