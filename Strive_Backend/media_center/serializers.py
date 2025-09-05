f4761e198860afffbccaf5c6eaf5310b72525713
Uzair <mukhan3223@gmail.com>
2025-09-05 14:40:06 +0500

    Serializer added

--- Strive_Backend/media_center/serializers.py
+++ Strive_Backend/media_center/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MediaPost, GalleryItem
+from .models import MediaPost, GalleryItem, DocumentItem
 
 
 class MediaPostSerializer(serializers.ModelSerializer):
