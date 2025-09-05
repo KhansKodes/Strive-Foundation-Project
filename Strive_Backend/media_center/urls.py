b8891726bcba93e99039b40cd5eb511f36a0aba4
Uzair <mukhan3223@gmail.com>
2025-09-05 14:42:40 +0500

    urls updated

--- Strive_Backend/media_center/urls.py
+++ Strive_Backend/media_center/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (FeaturedStoriesList, PressReleasesList, MediaPostDetail,
-       GalleryPhotosList, GalleryVideosList, GalleryCampaignList, GalleryCombinedList)
+       GalleryPhotosList, GalleryVideosList, GalleryCampaignList, GalleryCombinedList,
+       FlyersList, ReportsList, AuditsList, DocsCombined)
 
 app_name = "media_center"
 
@@ -8,8 +9,14 @@ urlpatterns = [
     path("featured-stories/", FeaturedStoriesList.as_view(), name="featured-stories"),
     path("press-releases/", PressReleasesList.as_view(), name="press-releases"),
     path("post/<slug:slug>/", MediaPostDetail.as_view(), name="post-detail"),  # optional
+    
     path("gallery/photos/", GalleryPhotosList.as_view(), name="gallery-photos"),
     path("gallery/videos/", GalleryVideosList.as_view(), name="gallery-videos"),
     path("gallery/campaign-highlights/", GalleryCampaignList.as_view(), name="gallery-campaign"),
     path("gallery/", GalleryCombinedList.as_view(), name="gallery-combined"),  # optional
+    
+    path("docs/flyers/", FlyersList.as_view(), name="docs-flyers"),
+    path("docs/reports/", ReportsList.as_view(), name="docs-reports"),
+    path("docs/audit/", AuditsList.as_view(), name="docs-audit"),
+    path("docs/", DocsCombined.as_view(), name="docs-combined"),  # optional
 ]
